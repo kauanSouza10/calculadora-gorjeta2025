62d99a03abbb381bc28fe1addae308dee567a24b
kauam.souzzaa <kauam.souzzaa@gmail.com>
2025-04-05 14:42:32 +0000

    início da lógica dos botões da porcentagem da gorjeta

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.getElementById("#bill").valueAsNumber
@@ -8,4 +9,11 @@ function receiveBillValue() {
 
 function receiveBillValue() {
     numberOfPeople = document.querySelector("#peaple").valueAsNumber
+}
+
+function receiveTipPercentageValue(value) {
+    tipPercentage = value / 100
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
 }
