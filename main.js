544bf806b49a5923a63beafc9e57cab9e3b6b034
kauam.souzzaa <kauam.souzzaa@gmail.com>
2025-04-05 13:58:50 +0000

    criação do javascript da função para receber o valor da conta e da função para receber o número de pessoas

---
+++ main.js
@@ -0,0 +1,11 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+
+function receiveBillValue() {
+    bill = document.getElementById("#bill").valueAsNumber
+}
+
+function receiveBillValue() {
+    numberOfPeople = document.querySelector("#peaple").valueAsNumber
+}
